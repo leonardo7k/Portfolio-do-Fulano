25263c289a13d912559ccd4ca3172ac6fb44cda2
leonardo7k <68858787+leonardo7k@users.noreply.github.com>
2021-03-05 08:18:04 -0300

    Initial Commit 2

---
+++ assets/js/portfolio.js
@@ -0,0 +1,78 @@
+var a = window.document.getElementById('specialization1')
+function hover(){
+    a.style.borderStyle = 'solid'
+    a.style.borderWidth = '2px'
+    a.style.borderColor = 'blue'
+    a.style.transition = '0.1 ease-in'
+}
+
+function out_hover(){
+    a.style.borderStyle = 'none'
+}
+
+
+var b = window.document.getElementById('specialization2')
+function hover2(){
+    b.style.borderStyle = 'solid'
+    b.style.borderWidth = '2px'
+    b.style.borderColor = 'blue'
+    b.style.transition = '0.1 ease-in'
+}
+
+function out_hover2(){
+    b.style.borderStyle = 'none'
+}
+
+
+var c = window.document.getElementById('specialization3')
+function hover3(){
+    c.style.borderStyle = 'solid'
+    c.style.borderWidth = '2px'
+    c.style.borderColor = 'blue'
+    c.style.transition = '0.1 ease-in'
+}
+
+function out_hover3(){
+    c.style.borderStyle = 'none'
+}
+
+
+var d = window.document.getElementById('specialization4')
+function hover4(){
+    d.style.borderStyle = 'solid'
+    d.style.borderWidth = '2px'
+    d.style.borderColor = 'blue'
+    d.style.transition = '0.1 ease-in'
+}
+
+
+function out_hover4(){
+    d.style.borderStyle = 'none'
+}
+
+
+
+var e = window.document.getElementById('specialization5')
+function hover5(){
+    e.style.borderStyle = 'solid'
+    e.style.borderWidth = '2px'
+    e.style.borderColor = 'blue'
+    e.style.transition = '0.1 ease-in'
+}
+
+function out_hover5(){
+    e.style.borderStyle = 'none'
+}
+
+
+var f = window.document.getElementById('specialization6')
+function hover6(){
+    f.style.borderStyle = 'solid'
+    f.style.borderWidth = '2px'
+    f.style.borderColor = 'blue'
+    f.style.transition = '0.1 ease-in'
+}
+
+function out_hover6(){
+    f.style.borderStyle = 'none'
+}
---
+++ assets/js/scroll_smooth_effect.js
@@ -0,0 +1,11 @@
+$('.menu a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href');
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 69
+    }, 800);
+
+        console.log(targetOffset);
+});
+
